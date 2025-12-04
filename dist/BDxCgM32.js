042e17e392cc2e505d724f043727c0755e81c3e8
jin kyung <1day_1000@finger.co.kr>
2025-12-04 17:42:47 +0900

    fix: 이미지 경로 수정

--- dist/BDxCgM32.js
+++ dist/BDxCgM32.js
@@ -5,7 +5,7 @@ const o = [
     tags: ["next.js", "figma"],
     title: "Baas 플랫폼 개발",
     desc: "블록체인을 모르는 사용자들도 블록체인 서비스를 쉽게 경험할 수 있는 서비스형 플랫폼 웹을 Next.js로 구축하였습니다.",
-    image: "/f-baas.png",
+    image: "/portfolio/f-baas.png",
     noImage: !1,
   },
   {
@@ -13,7 +13,7 @@ const o = [
     tags: ["nuxt.js", "pwa"],
     title: "DID 프로젝트",
     desc: "PWA 기반의 DID(Decentralized Identity, 분산신원확인) 웹 페이지 개발 후, 웹 앱(IOS)로 리팩토링하였습니다.",
-    image: "/fdid.png",
+    image: "/portfolio/fdid.png",
     noImage: !1,
   },
   {
@@ -21,7 +21,7 @@ const o = [
     tags: ["nuxt.js"],
     title: "비상장 토큰 증권 플랫폼 개발",
     desc: "비상장 토큰을 거래할 수 있는 거래 플랫폼 웹과 트랜잭션을 추적할 수 있는 모니터링 웹을 개발하였습니다.",
-    image: "/trade.png",
+    image: "/portfolio/trade.png",
     noImage: !1,
   },
   {
@@ -29,7 +29,7 @@ const o = [
     tags: ["nuxt.js"],
     title: "f-asset 플랫폼 웹 개발",
     desc: "이용자 각종 자산을 충전하고 특정 자산으로 결제/송금 처리가 가능한 플랫폼 웹을 개발하였습니다.",
-    image: "/fplatform.png",
+    image: "/portfolio/fplatform.png",
     noImage: !1,
   },
   {
